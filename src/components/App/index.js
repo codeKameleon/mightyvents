71ce68e30f4e88a4d067e61a8554d5924b421ae2
Axel Tavormina <tavormina.axel@gmail.com>
2019-04-22 18:17:27 +0200

    Set up redux

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import AppRouter from '../AppRouter';
+import { Provider } from 'react-redux';
+import store from '../../store';
 import styles from './styles.module.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className={styles.App}>
-        <AppRouter />
-      </div>
+      <Provider store={store}>
+        <div className={styles.App}>
+          <AppRouter />
+        </div>
+      </Provider>
     );
   }
 }
